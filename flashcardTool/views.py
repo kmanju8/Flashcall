5ffee7bbecb6aa7c868e6afe64cc798f3c706e96
Ben <benlm1@hotmail.com>
2021-04-01 11:04:39 +0100

    added basic pages needed for backend

--- flashcardTool/views.py
+++ flashcardTool/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def homepage(request):
     return HttpResponse('You are on the homepage')
+
+def login(request):
+    return HttpResponse('Login page')
+
+def profile(request): 
+    return HttpResponse('Profile page - view cards and decks')
+
+def addCardsAndDecks(request): 
+    return HttpResponse('Make cards and add them to decks on this page')
+
+
+
